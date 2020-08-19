8bbfdf6da556b93c2120c6e616291210cd631b8d
Adrian Sochaniwsky <6884645+adrian-soch@users.noreply.github.com>
2020-08-19 10:49:46 -0400

    Added gps test

---
+++ test/gps_razorIMU_test.ino
@@ -0,0 +1,84 @@
+/*
+    Hardware: Sparkfun Razor 9dof IMU M0
+              NEO 6M GPS
+              UART Serial connection
+*/
+
+#include <TinyGPS++.h>
+
+static const uint32_t GPSBaud = 9600;
+
+// The TinyGPS++ object
+TinyGPSPlus gps;
+
+
+void setup()
+{
+  SerialUSB.begin(115200);
+  Serial1.begin(GPSBaud);
+}
+
+void loop()
+{
+  // This sketch displays information every time a new sentence is correctly encoded.
+  while (Serial1.available() > 0)
+    if (gps.encode(Serial1.read()))
+      displayInfo();
+
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+  {
+    SerialUSB.println(F("No GPS detected: check wiring."));
+    while(true);
+  }
+}
+
+void displayInfo()
+{
+  SerialUSB.print(F("Location: ")); 
+  if (gps.location.isValid())
+  {
+    SerialUSB.print(gps.location.lat(), 6);
+    SerialUSB.print(F(","));
+    SerialUSB.print(gps.location.lng(), 6);
+  }
+  else
+  {
+    SerialUSB.print(F("INVALID"));
+  }
+
+  SerialUSB.print(F("  Date/Time: "));
+  if (gps.date.isValid())
+  {
+    SerialUSB.print(gps.date.month());
+    SerialUSB.print(F("/"));
+    SerialUSB.print(gps.date.day());
+    SerialUSB.print(F("/"));
+    SerialUSB.print(gps.date.year());
+  }
+  else
+  {
+    SerialUSB.print(F("INVALID"));
+  }
+
+  SerialUSB.print(F(" "));
+  if (gps.time.isValid())
+  {
+    if (gps.time.hour() < 10) SerialUSB.print(F("0"));
+    SerialUSB.print(gps.time.hour());
+    SerialUSB.print(F(":"));
+    if (gps.time.minute() < 10) SerialUSB.print(F("0"));
+    SerialUSB.print(gps.time.minute());
+    SerialUSB.print(F(":"));
+    if (gps.time.second() < 10) SerialUSB.print(F("0"));
+    SerialUSB.print(gps.time.second());
+    SerialUSB.print(F("."));
+    if (gps.time.centisecond() < 10) SerialUSB.print(F("0"));
+    SerialUSB.print(gps.time.centisecond());
+  }
+  else
+  {
+    SerialUSB.print(F("INVALID"));
+  }
+
+  SerialUSB.println();
+}
